2f75f850212cfed07b6f24e8f92946830df22d5a
mikitseg <tsegayemikiyas9@gmail.com>
2024-08-29 02:13:29 +0300

    creating new page

--- core/settings.py
+++ core/settings.py
@@ -98,11 +98,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # Basket session ID
 BASKET_SESSION_ID = 'basket'
 
-# Stripe Payment
-os.environ.setdefault('STRIPE_PUBLISHABLE_KEY', 'pk_test_51IHxTTJm9Ogh6om63DrCDlFfxcPDTvbUVy1CoSiI1ZS2GKt8UJojUJlbo9CAOCHnNgEqwKlQlnv9TmyzKIUUkr8800w8nNvBfw')
-STRIPE_SECRET_KEY = 'sk_test_51IHxTTJm9Ogh6om6ryBhjePFWUTXvweI5y5gXjFhgPWVztF83X6Rhae1LGfW8bteV5ebb2KhX9w61Q1117Sw1iHE00gzR7PmNq'
-# stripe listen --forward-to localhost:8000/payment/webhook/
-
 # Custom user model
 AUTH_USER_MODEL = 'account.UserBase'
 LOGIN_REDIRECT_URL = '/account/dashboard'
